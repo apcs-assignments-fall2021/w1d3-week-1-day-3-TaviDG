0cf309d1742d05e1cb16bc183d15d99c0606c442
Tavi Greenfield <tavi@elyandpilar.com>
2021-09-03 14:33:52 -0700

    Wrote math method and random code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,12 +4,19 @@ public class MyMain {
     public static int add5(int x) {
         return (x + 5);
     }
-
+    public static boolean checkBigger(int x, int y) {return (y>x);}
     // Given three ints x, y, and z, this method
     // returns the largest of the three numbers
     public static int findBiggestNumber(int x, int y, int z) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int biggest = x;
+        if (checkBigger(biggest,y)){
+            biggest=y;
+        }
+
+        if (checkBigger(biggest,z)){
+            biggest=z;
+        }
+        return biggest;
     }
 
     // This method simulates the flip of a coin, where
@@ -17,7 +24,7 @@ public class MyMain {
     // This method should return true half the time, and false the other half
     public static boolean flipCoin() {
         // REPLACE THIS WITH YOUR CODE
-        return false;
+        return (Math.random()>=0.5);
     }
 
     // This method simulates the flip of a weighted coin,
@@ -30,13 +37,13 @@ public class MyMain {
     // This method rounds a double to the nearest whole number
     public static int round(double d) {
         // REPLACE THIS WITH YOUR CODE
-        return 0;
+        return (int) (d+0.5);
     }
 
     // This method returns a random number between 1 and 6, inclusive
     public static int rollDie() {
         // REPLACE THIS WITH YOUR CODE
-        return 0;
+        return (int) ((6*Math.random())+1);
     }
 
     // This method carries out the quadratic formula and *prints* out the answers
@@ -49,7 +56,14 @@ public class MyMain {
         int x = 2;
         int y = add5(x);
         System.out.println(y);
+        System.out.println(findBiggestNumber(2,5,3));
+        if (flipCoin()){
+            System.out.println("heads");
+        }
+        else{
+            System.out.println("tails");
+        }
+        System.out.println(rollDie());
 
-        // YOUR CODE HERE
     }
 }
